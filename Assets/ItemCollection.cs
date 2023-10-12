30b3b91b60f103edac560602c9fd377e7aba5314
DJ Brown <darre@LAPTOP-KGJ4L82U.mit.edu>
2023-10-12 16:59:44 -0400

    We made a second player. Item effect is lowkey broken

---
+++ Assets/ItemCollection.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ItemCollection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Boolean ItemOne;
+    void Start()
+    {
+        ItemOne = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject otherPlayer = GameObject.Find("PlayaTwo");
+        if (ItemOne= true)
+        {
+            if (Input.GetKey(KeyCode.L))
+            {
+                Debug.Log("speedup");
+                otherPlayer.GetComponent<PlayerTwoMove>().testfn();
+                ItemOne = false;
+            }
+            else
+            {
+                ItemOne = false;
+                Debug.Log("else");
+            }
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name == "ItemOne")
+        {
+            Debug.Log("Collected Item 1");
+            ItemOne = true;
+            GameObject Triangle = GameObject.Find("ItemOne");
+            Triangle.GetComponent<SpriteRenderer>().enabled = false;
+
+        }
+        //Playa.gameObject.SetActive(false);
+    }
+}
